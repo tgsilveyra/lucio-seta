36f3f4e736c413125f88360d929a66d802a0ae78
Tomas Silveyra <tomas.silveyra@mediamonks.com>
2024-11-03 22:07:32 -0300

    Test absolute url

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "development" ? "" : "/lucio_seta",
   env: {
-    BASE_PATH: process.env.NODE_ENV === "development" ? "" : "/lucio_seta",
+    BASE_PATH:
+      process.env.NODE_ENV === "development"
+        ? ""
+        : "https://tgsilveyra.github.io/lucio_seta",
   },
 };
 
