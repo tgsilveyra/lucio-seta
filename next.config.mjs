1298a618d53b23c8455dbb1135e9b6ea94c05739
Tomas Silveyra <tomas.silveyra@mediamonks.com>
2024-11-03 21:59:49 -0300

    Add basePath config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "development" ? "" : "/lucio_seta",
+  env: {
+    BASE_PATH: process.env.NODE_ENV === "development" ? "" : "/lucio_seta",
+  },
 };
 
 export default nextConfig;
