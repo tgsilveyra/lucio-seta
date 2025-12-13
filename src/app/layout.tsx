57b0dd7a76c9803ade183efa8ea9a8c1ab091b8a
tomassilveyra <tomas.silveyra@monks.com>
2025-12-13 15:35:07 -0300

    Update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body
         className={`${inter.variable} ${ibm.variable} ${fontRoom.variable}`}
       >
